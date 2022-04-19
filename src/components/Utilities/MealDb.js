cb9c75cf6c31892e4598684c161e9ce63abff243
CsdSreekanta <sreekanta.csd@gmail.com>
2022-04-20 01:03:36 +0600

    local storage fully updated

--- src/components/Utilities/MealDb.js
+++ src/components/Utilities/MealDb.js
@@ -1,14 +1,27 @@
 const addToDb = id =>{
-    const quantity = localStorage.getItem(id)
-    
+let shoppingCart ={};
+// get the shopping cart
+const storedCart = localStorage.getItem('shopping-cart');
+if(storedCart){
+ shoppingCart =JSON.parse(storedCart)
+}
+else{   
+    shoppingCart ={}
+}
+
+
+// add quantity
+    const quantity = shoppingCart[id]
     if(quantity){
-        const newQuantity = parseInt(quantity) + 1;
-        localStorage.setItem(id, newQuantity)
+        const newQuantity = (quantity) + 1;
+        shoppingCart[id] =newQuantity;
+        // localStorage.setItem(id, newQuantity)
     }
     else{
-        localStorage.setItem(id, 1)
+        // localStorage.setItem(id, 1)
+        shoppingCart[id]=1
     }
-  
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart))
 }
 
 export {addToDb}
